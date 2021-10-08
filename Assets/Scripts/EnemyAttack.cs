45f1159e5a871308346d52a61ccfd025556a54d9
Robert <robertjwisniowski@gmail.com>
2021-10-08 18:05:05 +0200

    The Player's health and the enemy's attack system have been added.

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAttack : MonoBehaviour
+{
+    PlayerHealth target;
+    [SerializeField] float damage = 40f;
+
+    NavMeshAgent navMeshAgent;
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        target = FindObjectOfType<PlayerHealth>();
+    }
+
+    public void AttackHitEvent()
+    {
+        if (target == null) return;
+        Debug.Log("Feel the fury of mighty duck!");
+            target.TakeDamage(damage);
+    }
+
+}
