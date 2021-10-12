977aa5d441f23a06b6672cc3445f3587816987a3
Robert <robertjwisniowski@gmail.com>
2021-10-12 21:39:30 +0200

    A death screen has been added.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,12 +6,15 @@ public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] float hitPoints = 100;
 
+
     public void TakeDamage(float damage)
     {
         hitPoints -= damage;
-        if(hitPoints>=0)
+        if(hitPoints<=0)
         {
             Debug.Log("You are dead");
+
+            GetComponent<DeathHandler>().HandleDeath();
         }
     }
 }
