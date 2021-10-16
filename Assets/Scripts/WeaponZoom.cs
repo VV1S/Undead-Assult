4ce25b9df44f90e79471762485a7688c5f68a7a8
Robert <robertjwisniowski@gmail.com>
2021-10-16 14:59:26 +0200

    The zoom In bug has been fixed.

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -16,24 +16,28 @@ public class WeaponZoom : MonoBehaviour
 
     bool zoomedInToggle = false;
 
+    private void OnDisable()
+    {
+        ZoomOut();
+    }
+
     private void Update()
     {
         if(Input.GetMouseButtonDown(1))
         {
            if( zoomedInToggle == false)
             {
-                zoomedInToggle = true;
                 ZoomIn();
             }
            else
             {
-                zoomedInToggle = false;
                 ZoomOut();
             }
         }
     }
     void ZoomIn()
     {
+        zoomedInToggle = true;
         fpsCamera.fieldOfView = zoomedInFOV;
         fpsController.mouseLook.XSensitivity = zoomInSensitivity;
         fpsController.mouseLook.YSensitivity = zoomInSensitivity;
@@ -41,6 +45,7 @@ public class WeaponZoom : MonoBehaviour
 
     void ZoomOut()
     {
+        zoomedInToggle = false;
         fpsCamera.fieldOfView = zoomedOutFOV;
         fpsController.mouseLook.XSensitivity = zoomOutSensitivity;
         fpsController.mouseLook.YSensitivity = zoomOutSensitivity;
